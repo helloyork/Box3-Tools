9df25a1c8a2461f8add0afb99a83ded58f0bb6a7
York <111544765+helloyork@users.noreply.github.com>
2023-06-20 21:35:31 -0700

    t

--- src/数据库工具/NomenSQLitePro/index.js
+++ src/数据库工具/NomenSQLitePro/index.js
@@ -11,6 +11,77 @@ class NomenSQLitePro {
         if (NomenSQLitePro.instance) return NomenSQLitePro.instance;
         NomenSQLitePro.instance = this;
     }
+    static instance = null;
+    static plugins = {};
+    static pluginUsing = false;
+    static LaunchAction = {
+        CREATE_OR_LAUNCH: 'createlaunch',
+        LAUNCH: 'launch'
+    }
+    static DataType = {
+        NUMBER: 'INTEGER',
+        FLOAT: 'REAL',
+        TEXT: 'TEXT',
+        NULL: 'NULL',
+        BLOB: 'BLOB'
+    }
+    static launched = false;
+    static debug = false;
+    static getInstance() {
+        if (!this.instance) new this();
+        return this.instance;
+    }
+    static async launch(config, action, callback) {
+        if (!config) throw new Error('请传入配置参数后重试');
+        if (!this.instance) this.instance = new NomenSQLitePro();
+        else return this.instance;
+        let time = Date.now()
+        this.tables = config.tables || {};
+        this._data = {};
+        this.debug = config.debug || false;
+        this.warn = config.warn || false;
+        this.info = config.info || false;
+        this.pluginUsing = Array.isArray(config.plugins);
+        if (!Object.keys(this.tables).length) this._data = null;
+        Object.keys(this.tables).forEach(v => {
+            if (Array.isArray(this.tables[v]) && this.tables[v].length) {
+                this._data[v] = [];
+                this.tables[v].forEach(r => {
+                    if (typeof r.name == "string" && Object.values(this.DataType).includes(r.type)) {
+                        this._data[v].push({ name: r.name, type: r.type, notnull: r.notnull || false });
+                    }
+                })
+            }
+        })
+        if (NomenSQLitePro.debug) console.log(`[DEBUG]${JSON.stringify(this._data)}`);
+        if (!Object.keys(this._data).length) return this.instance;
+        if (this.pluginUsing) await this._subscribePlugin(config.plugins);
+        let errMessage = null;
+        if (typeof action == "string" && action.includes('create')) {
+            for (let i = 0; i < Object.keys(this._data).length; i++) {
+                let eString = `CREATE TABLE IF NOT EXISTS "${Object.keys(this._data)[i]}" 
+                (${this._data[Object.keys(this._data)[i]].reduce((p, c) => {
+                    let str = `"${c.name}" ${c.type} ${c.notnull ? 'NOT NULL' : ''}`;
+                    return p ? p.concat(',', str) : str;
+                }, false)})`;
+                try { await this.instance._exec(eString); } catch (err) {
+                    errMessage.concat(err);
+                }
+            }
+        }
+        if (config.autoUpdateColumn) {
+            try { await this.instance.update() } catch (err) {
+                console.error(`[ERROR]SQL NomenSQLitePro Error: ${err}`)
+            }
+        }
+        this.launched = true;
+        if (typeof callback == "function") callback(null, errMessage);
+        if (NomenSQLitePro.info) console.log(`[INFO] NomenSQLitePro启动完成 耗时：${Date.now() - time}ms`)
+        return this.instance;
+    }
+    _exec(cmd) {
+        return (typeof cmd == 'string') ? db.sql([cmd]) : new Promise(resolve => resolve(new TypeError('传入参数有误，预期应该为string')))
+    }
     async update() {
         function parseSql(sql) {
             let _sql = sql.match(/\(([\s\S]*)\)/)[1].replace(/\n/g, '');
@@ -38,63 +109,6 @@ class NomenSQLitePro {
         }
         if (NomenSQLitePro.info) console.log(`[INFO]Update Finish`)
     };
-    static launched = false;
-    static debug = false;
-    static async launch(config, action, callback) {
-        if (!config) throw new Error('请传入配置参数后重试');
-        if (!this.instance) this.instance = new NomenSQLitePro();
-        else return this.instance;
-        let time = Date.now()
-        this.tables = config.tables || {};
-        this._data = {};
-        this.debug = config.debug || false;
-        this.warn = config.warn || false;
-        this.info = config.info || false;
-        if (!Object.keys(this.tables).length) this._data = null;
-        Object.keys(this.tables).forEach(v => {
-            if (Array.isArray(this.tables[v]) && this.tables[v].length) {
-                this._data[v] = [];
-                this.tables[v].forEach(r => {
-                    if (typeof r.name == "string" && Object.values(this.DataType).includes(r.type)) {
-                        this._data[v].push({ name: r.name, type: r.type, notnull: r.notnull || false });
-                    }
-                })
-            }
-        })
-        if (NomenSQLitePro.debug) console.log(`[DEBUG]${JSON.stringify(this._data)}`);
-        if (!Object.keys(this._data).length) return this.instance;
-        let errMessage = null;
-        if (typeof action == "string" && action.includes('create')) {
-            for (let i = 0; i < Object.keys(this._data).length; i++) {
-                let eString = `CREATE TABLE IF NOT EXISTS "${Object.keys(this._data)[i]}" 
-                (${this._data[Object.keys(this._data)[i]].reduce((p, c) => {
-                    let str = `"${c.name}" ${c.type} ${c.notnull ? 'NOT NULL' : ''}`;
-                    return p ? p.concat(',', str) : str;
-                }, false)})`;
-                try { await this.instance._exec(eString); } catch (err) {
-                    errMessage.concat(err);
-                }
-            }
-        }
-        if (config.autoUpdateColumn) {
-            try { await this.instance.update() } catch (err) {
-                console.error(`[ERROR]SQL NomenSQLitePro Error: ${err}`)
-            }
-        }
-        this.launched = true;
-        if (typeof callback == "function") callback(null, errMessage);
-        if (NomenSQLitePro.info) console.log(`[INFO] NomenSQLitePro启动完成 耗时：${Date.now() - time}ms`)
-        return this.instance;
-    }
-    _exec(cmd) {
-        return (typeof cmd == 'string') ? db.sql([cmd]) : new Promise(resolve => resolve(new TypeError('传入参数有误，预期应该为string')))
-    }
-    getAllTables() {
-        return Object.keys(NomenSQLitePro._data);
-    }
-    async getAllTablesInDatabase() {
-        return (await this._exec(`SELECT * FROM sqlite_master`)).map(v => v.name)
-    }
     async _updateColumn(data, name) {
         function g(length) {
             var result = '';
@@ -120,9 +134,9 @@ class NomenSQLitePro {
     }
     operate(name) {
         let t = this;
-        if (typeof name == "string" && NomenSQLitePro.launched) return {
+        if (typeof name == "string") return {
             select(column, value) {
-                return t._exec(`SELECT * FROM "${name}" ${column == "*" ? `` : (`WHERE "${column}" = ` + `"${value}"`)}`)
+                return t._exec(`SELECT * FROM "${name}" ${column == "*" ? `` : (`WHERE "${column}" = ` + `"${value}"`)}`);
             },
             remove(column, value) {
                 return t._exec(`DELETE FROM "${name}" ${column == "*" ? `` : (`WHERE "${column}" = ` + `"${value}"`)}`)
@@ -144,17 +158,44 @@ class NomenSQLitePro {
         }
         return {};
     }
-    static instance = null;
-    static LaunchAction = {
-        CREATE_OR_LAUNCH: 'createlaunch',
-        LAUNCH: 'launch'
+    static async evalAll(fc, cg) {
+        let rc = [];
+        Object.keys(this.plugins).map(v => (this.plugins[v][fc] || (() => { }))).forEach(v => rc.push(new Promise(r => r(v({ NomenSQLitePro, ...cg })))));
+        let result = await new Promise((resolve, reject) => {
+            Promise.all([...rc, new Promise((r, j) => setTimeout(() => j('Time out'), 12 * 1000))])
+                .then(() => resolve({ ok: true }))
+                .catch(reason => reject({ reason, ok: false }));
+        });
+        if (!result.ok && result.reason == 'Time out') console.error('[ERROR] NomenSQLitePro Plugins Time out 插件执行超时');
+        else if (!result.ok) console.error(`[ERROR] NomenSQLitePro Plugins Error 插件执行错误\n${result.reason}`);
+        return;
     }
-    static DataType = {
-        NUMBER: 'INTEGER',
-        FLOAT: 'REAL',
-        TEXT: 'TEXT',
-        NULL: 'NULL',
-        BLOB: 'BLOB'
+    static _subscribePlugin(plugins) {
+        if (NomenSQLitePro.warn) console.warn(`[WARNING] 正在尝试载入插件，NomenSQLitePro无法保证插件是否具有危害，NomenSQLitePro对于载入具有危害的插件导致的问题不负责任`);
+        try {
+            if (!plugins || !Array.isArray(plugins) || !plugins.length || plugins.some(v => typeof v != "object")) return;
+            plugins.forEach((v, i) => {
+                if (typeof v.name == "string" && this.plugins[v.name] === undefined && typeof v.version == "string") {
+                    this.plugins[v.name] = {};
+                    Object.defineProperty(this.plugins[v.name], 'id', {
+                        get() { return i },
+                        set(v) { return v }
+                    });
+                }
+                else {
+                    if (NomenSQLitePro.warn) console.warn(`[WARNING] ID为${i}的插件载入失败，请检查插件注册格式`)
+                    return;
+                }
+                Object.assign(this.plugins[v.name], { ...v, log: "" });
+                if (NomenSQLitePro.warn && (this.plugins[v.name].onLoad && (this.plugins[v.name].onLoad.toString.includes('delete ') || this.plugins[v.name].onLoad.toString.includes('while')))) {
+                    console.warn(`[WARNING] 插件"${v.name}"(ID:${i})可能存在有害行为，请进行检查`);
+                };
+                if (this.plugins[v.name].onLoad) this.plugins[v.name].onLoad({ NomenSQLitePro });
+                if (NomenSQLitePro.info) console.log(`[INFO] 插件"${v.name}"(ID:${i})载入成功`);
+            })
+        } catch (err) {
+            console.error(`[ERROR]SQL NomenSQLitePro 尝试载入插件时发生错误: ${err}`);
+        }
     }
 }
 
@@ -177,7 +218,16 @@ const NomenSQLiteProConfig = {
     info: false, /* 是否显示标识信息 */
     autoUpdateColumn: false, /* 是否自动更新表格 */
     tables: tablesData, /* 传入表格结构 */
+    plugins: [], /* 传入插件Obj信息[] */
 };
+
+const ExamplePlugin = {
+    name:"ExamplePlugin",
+    version:"v1.0.0",
+    onLoad(){
+        console.log('hi! Plugin loading');
+    }
+}
 
 await NomenSQLitePro.launch(NomenSQLiteProConfig, NomenSQLitePro.LaunchAction.CREATE_OR_LAUNCH, (e, r) => console.log(r));
 
